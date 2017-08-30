5fb385f76e6ee82c0002301927769ae4f6255073
Erika Weil <erikaweil@outlook.com>
2017-08-30 16:34:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,8 +9,8 @@
 var playlist = {artist: song};
 
 function updatePlaylist(playlist, artist, song) {
-  playlist[artist] = song; 
-  return playlist; 
+  playlist[artist] = song;
+  return playlist;
 }
 
 function removeFromPlaylist(playlist, artist) {
